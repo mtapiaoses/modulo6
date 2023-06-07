8d57777e868296ff632de4a2f7c0d2e8528cb29e
CRIS-LI-DEV <131208714+CRIS-LI-DEV@users.noreply.github.com>
2023-06-07 00:07:54 -0400

    Actualización modulo 6

--- telovendo_django_2/proyecto/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from app.models import PerfilUsuario, Proveedores
-# Register your models here.
-
-admin.site.register(PerfilUsuario)
-admin.site.register(Proveedores)
--- telovendo_django_2/proyecto/app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from app.models import Proveedores
-
-
-
-class ProveedoresForm(forms.ModelForm):
-    class Meta: 
-        model = Proveedores
-        fields = ['nombre','razon_social','telefono','email','rut']
